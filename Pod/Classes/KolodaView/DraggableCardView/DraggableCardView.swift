6674f00a2a9280a0b9909829ceecef6c0918d9b6
Nicolas <ntorres@codigodelsur.com>
2020-11-03 09:57:31 -0300

    fix compile error

--- Pod/Classes/KolodaView/DraggableCardView/DraggableCardView.swift
+++ Pod/Classes/KolodaView/DraggableCardView/DraggableCardView.swift
@@ -283,7 +283,7 @@ public class DraggableCardView: UIView, UIGestureRecognizerDelegate {
         }
         
         // Ensure it's a horizontal drag
-        let velocity = gestureRecognizer.velocity(in: self)
+        let velocity = panGestureRecognizer.velocity(in: self)
         if abs(velocity.y) > abs(velocity.x) {
             return false
         }
